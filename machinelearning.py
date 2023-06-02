c5be6a701d37f6956e05052fff547c25257255fb
pass-pass-pass <forgithubuse@gmail.com>
2023-06-02 18:42:49 -0500

    no

---
+++ machinelearning.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+
+
+# k nearest method
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+
+
+knn_model = KNeighborsClassifier(n_neighbors=1)
+knn_model.fit(x_train, y_train)
+y_prediction = knn_model.predict(x_test)
+y_prediction
+
+from sklearn.naive_bayes import GaussianNB
